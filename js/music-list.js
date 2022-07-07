4733c08ff70e1936e0188842266b3dcccac87149
Harikrishna KP <harikp2002@gmail.com>
2022-07-07 15:43:03 +0530

    MyCustomMoosicPlayer

---
+++ js/music-list.js
@@ -0,0 +1,47 @@
+//instructions to add songs are at the bottom of this file
+
+let allMusic = [
+  {
+    name: "Notion",
+    artist: "The Rare Occassiona",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "The Less I Know The Better",
+    artist: "Tame Impala",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Driving South",
+    artist: "Goth Babe & Blood Cultures",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Crash My Car",
+    artist: "Coin",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Running Up That Hill",
+    artist: "Kate Bush",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Run The World!!!!",
+    artist: "Dayglow",
+    img: "music-6",
+    src: "music-6"
+  },
+  // paste like this to add more songs:
+  // {
+  //   name: <name>,
+  //   artist: <artist_name>,
+  //   img: <image_name> image should be in .jpg format,
+  //   src: <music_name> song must be in .mp3 format
+  // },
+];
